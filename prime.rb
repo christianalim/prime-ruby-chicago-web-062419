8c7b23f33def38bf9f0f2933705ec0f66370d6bd
Christian Lim <christiananthonylim1@gmail.com>
2019-05-24 23:49:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+def prime?(n)
+  case n
+
+  when 1
+    false
+  when 2
+    true
+  when 3
+    true
+  when n % 2 == 0
+    false
+  when n % 3 == 0
+    false
+  end
+
+  i = 5
+  w = 2
+
+  while i * i <= n
+    if n % i == 0
+      false
+    i += w
+    w = 6 - w
+    end
+  end
+  true
+end
