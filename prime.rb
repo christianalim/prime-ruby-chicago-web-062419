658872977a9448bd44bc948b238980c3ba3c5549
Christian Lim <christiananthonylim1@gmail.com>
2019-05-25 00:48:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(num)
   return false if num <= 1
-  (2..Math.sqrt(num)).each { |i|
+  (2..Math.sqrt(num)).each |i|
   if num % i == 0 && i < num
     return false
   end
-  }
+  
   true
 end
