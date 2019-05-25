69131bf87a8aecfb6876954ec4fa8c1707887433
Christian Lim <christiananthonylim1@gmail.com>
2019-05-25 00:09:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,12 @@ def prime?(n)
     true
   elsif n <= 1
     false
-  end
+  else
+    i = 2
+    array = (2..n).to_a
+    array.each do |i|
+      break if n % i == 0
+      false
+    end
+    true
 end
