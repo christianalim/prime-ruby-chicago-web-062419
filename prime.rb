40545ed473dde89a625d350449029fc8d2f1b27f
Christian Lim <christiananthonylim1@gmail.com>
2019-05-25 00:03:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,4 @@
 def prime?(n)
-  root = Math.sqrt(n)
-  array = (2..root).to_a
-  i = 2
-
-  if n <= 1
-    false
-  end
   if n == 2
     true
-  end
-
-  while i < root do
-    if root % i == 0
-      false
-    else
-      true
-    end
-  end
 end
