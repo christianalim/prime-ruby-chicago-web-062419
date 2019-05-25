ab848fa1bb7eaae33db592b3e9a69613b74f53f5
Christian Lim <christiananthonylim1@gmail.com>
2019-05-25 00:35:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,5 @@
-def prime?(n)
-  if n == 2 || n == 3
-    return true
-
-  elsif n < 2
-    false
-  else
-    i = 2
-    while i < n-1 do
-      if n % i == 0
-        return false
-        i += 1
-      else
-        return true
-        i += 1
-      end
-    end
-  end
+def prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
 end
