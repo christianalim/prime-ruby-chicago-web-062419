6853419300fdae299f9f3ff5011530c83b4f61fc
Christian Lim <christiananthonylim1@gmail.com>
2019-05-25 00:17:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,12 @@
 def prime?(n)
-  if n == 2 || n == 3
-    true
-  elsif n <= 1
+  if n < 2
     false
   else
     i = 2
     for i in 2..n-1
       if n % i == 0
         return false
+        i += 1
       else
         return true
       end
